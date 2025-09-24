1b7d84b03752da3406f3165209d425066310d0e4
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-24 10:08:35 +0530

    initial commit - button action

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [showPara, setShowPara] = useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={() => setShowPara(true)}>
+        Click Me
+      </button>
+      {showPara && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy.
+        </p>
+      )}
     </div>
   );
 }
